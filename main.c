45926b7a64c50b4082c21d6c411e9386df3c590d
Alan Gomez <157183000+KleperForest@users.noreply.github.com>
2024-04-10 22:37:45 -0600

    Initial commit

---
+++ main.c
@@ -0,0 +1,77 @@
+//*********************************************************************
+// Universidad del Valle de Guatemala
+// IE2023: Programación de Microcontroladores
+// Autor: Alan Gomez
+// Proyecto: POST_LAB_4_C.c
+// Descripción: Cuarto POST-Laboratorio de Programación de Microcontroladores con lenguaje C.
+// Hardware: ATmega328p
+// Created: 6/4/2024
+//*********************************************************************
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+volatile uint8_t contador = 0; // Declarar contador como volatile para uso en interrupción
+
+void setup(void);
+
+int main(void) {
+    cli(); // Deshabilitar Interrupciones
+
+    // Habilitar interrupciones de pin change para los pines PC0 y PC3
+	PCMSK1 |= ((1 << PCINT11) | (1 << PCINT8)); // Habilitar interrupciones para PCINT11 y PCINT8
+    PCICR |= (1 << PCIE1); // PCINT1 8-14 Grupo 1
+
+    setup(); // Configurar Puertos
+    sei(); // Habilitar Interrupciones Globales
+
+    while (1) {
+        // Programa principal
+		//Displays
+		//D1
+		PORTB |= (1 << PB1);// Encender transistor en PB1
+		PORTD = 0b00000001;// Mostrar primier display
+		_delay_ms(1);
+		PORTB &= ~(1 << PB1);// Apagar transistor en PB1
+		_delay_ms(1);
+		//D2
+		PORTB |= (1 << PB2);// Encender transistor en PB2
+		PORTD = 0b00000001;// Mostrar segundo display
+		_delay_ms(1);
+		PORTB &= ~(1 << PB2);// Apagar transistor en PB2
+		_delay_ms(1);
+		
+		// Actualizar el puerto D con el valor del contador
+		PORTB |= (1 << PB0);// Encender transistor en PB0
+		PORTD = contador;// Mostrar valor de contador
+		_delay_ms(1);
+		PORTB &= ~(1 << PB0);// Apagar transistor en PB0
+		_delay_ms(1);
+    }
+}
+
+void setup(void) {
+    // Configurar pines PB0 a PB2 como salidas para los transistores
+    DDRB |= 0b00000111;
+
+    // Configurar PC3 y PC0 como entradas con pull-up habilitado para los botones
+    DDRC &= ~((1 << DDC3) | (1 << DDC0)); // Configurar como entrada
+    PORTC |= ((1 << PORTC3) | (1 << PORTC0)); // Habilitar resistencias de pull-up en PC3 y PC0
+
+    // Configurar pines PD0 a PD7 como salidas para los LEDs
+    DDRD |= 0b11111111;
+}
+
+ISR(PCINT1_vect) {
+	_delay_ms(10);
+    // Verificar si la interrupción fue causada por PCINT11 (PC3)
+    if (PINC & (1 << PINC3)) {
+        contador++; // Incrementar contador si el botón en PC3 fue presionado
+    }
+    // Verificar si la interrupción fue causada por PCINT8 (PC0)
+    if (PINC & (1 << PINC0)) {
+        contador--; // Decrementar contador si el botón en PC0 fue presionado
+	}
+	contador = (contador > 255) ? 0 : contador;// Si contador Overflow 255 se resetea el contador
+	contador = (contador < 0) ? 255 : contador;// Si contador Underflow 0 se setea a 255
+}
